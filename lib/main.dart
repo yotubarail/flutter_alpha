984f94d66ae21784a5a7038449f78a81825c77b5
kou72 <genpokopokopoko@gmail.com>
2021-07-24 12:29:51 +0900

    key関係のエラー解決

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 // ルーティング
-// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
